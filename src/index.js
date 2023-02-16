c61ce22bfb0749a4b976a1adbe0337f8e8685cae
maria-kashpur <kashpur.maria@icloud.com>
2023-02-16 18:27:59 +0300

    feat: add function check(str, bracketsConfig)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const openSimbol = ['{'];
+  const pareSimbols = {
+     '}': '{'
+  };
+  let arr = [];
+  
+  bracketsConfig.forEach(el => {
+     openSimbol.push(el[0])
+     pareSimbols[el[1]] = el[0]
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let lastSimbol = arr[arr.length - 1]
+
+    if ((openSimbol.includes(currentSymbol))) {
+      (lastSimbol === currentSymbol && pareSimbols[currentSymbol] === currentSymbol) ? arr.pop() : arr.push(currentSymbol);
+    } else {
+      if (arr.length === 0) {
+        return false;
+      } else if (pareSimbols[currentSymbol] === lastSimbol) {
+        arr.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return arr.length === 0
 }
